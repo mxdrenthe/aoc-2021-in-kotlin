4794e905fd87ab57a35b2a67ed5bf37e2375e81d
Michel Drenthe <mixed84@gmail.com>
2021-12-07 10:05:04 +0100

    Finish day 7 convert code in more concise way of writing

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,50 +1,40 @@
 import kotlin.math.pow
 
 fun main() {
-    fun convertInput(input: List<String>) = input
+    fun getCrabs(input: List<String>) = input
         .flatMap {
             it.split(",")
                 .map { it.toInt() }
         }
 
     fun part1(input: List<String>): Long {
-        val crabs = convertInput(input)
+        val crabs = getCrabs(input)
         val min = crabs.minOf { it }.toLong()
         val max = crabs.maxOf { it }.toLong()
 
-        var fuel = Long.MAX_VALUE
-        for (position in min..max) {
-            val neededFuel = crabs.sumOf {
+        return (min..max).minOf { position ->
+            crabs.sumOf {
                 when (it < position) {
                     true -> position - it
                     false -> it - position
                 }
             }
-            if (neededFuel < fuel) {
-                fuel = neededFuel
-            }
         }
-        return fuel
     }
 
     fun part2(input: List<String>): Long {
-        val crabs = convertInput(input)
+        val crabs = getCrabs(input)
         val min = crabs.minOf { it }.toLong()
         val max = crabs.maxOf { it }.toLong()
 
-        var fuel = Long.MAX_VALUE
-        for (position in min..max) {
-            val neededFuel = crabs.sumOf {
+        return (min..max).minOf { position ->
+            crabs.sumOf {
                 when (it < position) {
                     true -> (position - it).let { distance -> (.5 * distance.toDouble().pow(2) + .5 * distance).toLong() }
                     false -> (it - position).let { distance -> (.5 * distance.toDouble().pow(2) + .5 * distance).toLong() }
                 }
             }
-            if (neededFuel < fuel) {
-                fuel = neededFuel
-            }
         }
-        return fuel
     }
 
     // test if implementation meets criteria from the description, like:
